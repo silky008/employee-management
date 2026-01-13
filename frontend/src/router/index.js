5eff7e07a5f4f8b24d236d236e47c8746498ff52
Silky <silky@localhost.com>
2026-01-13 14:44:31 +0100

    feat: add users route to vue router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "../views/LoginView.vue";
 import DashboardView from "../views/DashboardView.vue";
+import UsersView from "../views/UsersView.vue";
 
 const routes = [
   { path: "/login", component: LoginView },
   { path: "/", component: DashboardView, meta: { requiresAuth: true } },
+  { path: "/users", component: UsersView, meta: { requiresAuth: true } },
 ];
 
 const router = createRouter({
