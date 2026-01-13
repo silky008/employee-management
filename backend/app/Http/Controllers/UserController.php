317959ffc2a9e1b3dc90895989ef44ceffabd960
Silky <silky@localhost.com>
2026-01-13 12:17:19 +0100

    feat: restrict users list to admin using policies

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ class UserController extends Controller
 
     public function index()
     {
+        $this->authorize('viewAny', User::class);
         return User::with('role')->paginate(10);
     }
 }
