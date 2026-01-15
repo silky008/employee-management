11a98b812d997cd4250d7ed2ef3a3eda75b727d7
Silky <silky@localhost.com>
2026-01-15 12:13:37 +0100

    feat: add pagination support to users list

--- frontend/src/views/UsersView.vue
+++ frontend/src/views/UsersView.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
     <h2>Users</h2>
-    <table>
+    <table border="1" cellpadding="1">
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+      </tr>
       <tr v-for="user in users.data" :key="user.id">
         <td>{{ user.name }}</td>
         <td>{{ user.email }}</td>
         <td>{{ user.role.name }}</td>
       </tr>
     </table>
+    <!--Pagination-->
+    <div style="margin-top: 20px">
+      <button :disabled="page === 1" @click="changePage(page - 1)">Pre</button>
+      <span style="margin: 0 10px"> Page {{ page }} of {{ lastPage }} </span>
+      <button :disabled="page === lastPage" @click="changePage(page + 1)">
+        Next
+      </button>
+    </div>
   </div>
 </template>
 
@@ -19,15 +32,25 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 const users = ref({ data: [] });
+const page = ref(1);
+const lastPage = ref(1);
 
-onMounted(async () => {
+const fetchUsers = async () => {
   try {
-    const res = await api.get("/users");
+    const res = await api.get(`/users?page=${page.value}`);
     users.value = res.data;
+    lastPage.value = res.data.meta.last_page;
   } catch (error) {
     if (error.response && error.response.status === 403) {
       router.push("/403");
     }
   }
-});
+};
+
+const changePage = (newPage) => {
+  page.value = newPage;
+  fetchUsers();
+};
+
+onMounted(fetchUsers);
 </script>
